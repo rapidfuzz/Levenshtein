c08adbbcea79ff171ac901a02b5e573c10df1210
Max Bachmann <max.bachmann@iracing.com>
2022-07-21 01:17:37 +0200

    replace editops implementation

--- src/Levenshtein/__init__.py
+++ src/Levenshtein/__init__.py
@@ -34,10 +34,6 @@ from rapidfuzz.distance import (
 
 from Levenshtein.levenshtein_cpp import (
     quickmedian,
-    inverse,
-    matching_blocks,
-    subtract_edit,
-    apply_edit,
     median,
     median_improve,
     setmedian,
@@ -75,11 +71,11 @@ def editops(*args):
         arg1, arg2, arg3 = args
         len1 = arg2 if isinstance(arg2, int) else len(arg2)
         len2 = arg3 if isinstance(arg3, int) else len(arg3)
-        return _Editops(arg1, len1, len2).as_list()
+        return _Editops(arg1, len1, len2)
 
     # find editops: we were called (s1, s2)
     arg1, arg2 = args
-    return _editops(arg1, arg2).as_list()
+    return _editops(arg1, arg2)
 
 
 def opcodes(*args):
@@ -113,8 +109,147 @@ def opcodes(*args):
         arg1, arg2, arg3 = args
         len1 = arg2 if isinstance(arg2, int) else len(arg2)
         len2 = arg3 if isinstance(arg3, int) else len(arg3)
-        return _Opcodes(arg1, len1, len2).as_list()
+        return _Opcodes(arg1, len1, len2)
 
     # find editops: we were called (s1, s2)
     arg1, arg2 = args
-    return _opcodes(arg1, arg2).as_list()
+    return _opcodes(arg1, arg2)
+
+
+def inverse(edit_operations):
+    """
+    Invert the sense of an edit operation sequence.
+
+    In other words, it returns a list of edit operations transforming the
+    second (destination) string to the first (source).  It can be used
+    with both editops and opcodes.
+
+    Parameters
+    ----------
+    edit_operations : list[]
+        edit operations to invert
+
+    Returns
+    -------
+    edit_operations : list[]
+        inverted edit operations
+
+    Examples
+    --------
+    >>> inverse(editops('spam', 'park'))
+    [('insert', 0, 0), ('delete', 2, 3), ('replace', 3, 3)]
+    >>> editops('park', 'spam')
+    [('insert', 0, 0), ('delete', 2, 3), ('replace', 3, 3)]
+    """
+    return edit_operations.inverse()
+
+
+def subtract_edit(edit_operations, subsequence):
+    """
+    Subtract an edit subsequence from a sequence.
+    
+    subtract_edit(edit_operations, subsequence)
+    
+    The result is equivalent to
+    editops(apply_edit(subsequence, s1, s2), s2), except that is
+    constructed directly from the edit operations.  That is, if you apply
+    it to the result of subsequence application, you get the same final
+    string as from application complete edit_operations.  It may be not
+    identical, though (in amibuous cases, like insertion of a character
+    next to the same character).
+    
+    The subtracted subsequence must be an ordered subset of
+    edit_operations.
+    
+    Note this function does not accept difflib-style opcodes as no one in
+    his right mind wants to create subsequences from them.
+
+    Examples
+    --------
+    >>> e = editops('man', 'scotsman')
+    >>> e1 = e[:3]
+    >>> bastard = apply_edit(e1, 'man', 'scotsman')
+    >>> bastard
+    'scoman'
+    >>> apply_edit(subtract_edit(e, e1), bastard, 'scotsman')
+    'scotsman'
+    """
+    return edit_operations - subsequence
+
+
+def apply_edit(edit_operations, source_string, destination_string):
+    """
+    Apply a sequence of edit operations to a string.
+    
+    apply_edit(edit_operations, source_string, destination_string)
+    
+    In the case of editops, the sequence can be arbitrary ordered subset
+    of the edit sequence transforming source_string to destination_string.
+    
+    Examples
+    --------
+    >>> e = editops('man', 'scotsman')
+    >>> apply_edit(e, 'man', 'scotsman')
+    'scotsman'
+    >>> apply_edit(e[:3], 'man', 'scotsman')
+    'scoman'
+    
+    The other form of edit operations, opcodes, is not very suitable for
+    such a tricks, because it has to always span over complete strings,
+    subsets can be created by carefully replacing blocks with 'equal'
+    blocks, or by enlarging 'equal' block at the expense of other blocks
+    and adjusting the other blocks accordingly.
+
+    >>> a, b = 'spam and eggs', 'foo and bar'
+    >>> e = opcodes(a, b)
+    >>> apply_edit(inverse(e), b, a)
+    'spam and eggs'
+    >>> e[4] = ('equal', 10, 13, 8, 11)
+    >>> e
+    [('delete', 0, 1, 0, 0), ('replace', 1, 4, 0, 3), ('equal', 4, 9, 3, 8), ('delete', 9, 10, 8, 8), ('equal', 10, 13, 8, 11)]
+    >>> apply_edit(e, a, b)
+    'foo and ggs'
+    """
+    return edit_operations.apply(source_string, destination_string)
+
+
+def matching_blocks(edit_operations, source_string, destination_string):
+    """
+    Find identical blocks in two strings.
+
+    Parameters
+    ----------
+    edit_operations : list[]
+        editops or opcodes created for the source and destination string
+    source_string : str | int
+        source string or the length of the source string
+    destination_string : str | int
+        destination string or the length of the destination string
+
+    Returns
+    -------
+    matching_blocks : list[]
+        List of triples with the same meaning as in SequenceMatcher's
+        get_matching_blocks() output.
+
+    Examples
+    --------
+    >>> a, b = 'spam', 'park'
+    >>> matching_blocks(editops(a, b), a, b)
+    [(1, 0, 2), (4, 4, 0)]
+    >>> matching_blocks(editops(a, b), len(a), len(b))
+    [(1, 0, 2), (4, 4, 0)]
+    
+    The last zero-length block is not an error, but it's there for
+    compatibility with difflib which always emits it.
+    
+    One can join the matching blocks to get two identical strings:
+
+    >>> a, b = 'dog kennels', 'mattresses'
+    >>> mb = matching_blocks(editops(a,b), a, b)
+    >>> ''.join([a[x[0]:x[0]+x[2]] for x in mb])
+    'ees'
+    >>> ''.join([b[x[1]:x[1]+x[2]] for x in mb])
+    'ees'
+    """
+    return _MatchingBlocks(edit_operations, source_string, destination_string)
