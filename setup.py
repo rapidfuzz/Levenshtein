8f6ea1a16c6497eb9039a5050eff1017efc89746
Max Bachmann <kontakt@maxbachmann.de>
2022-09-09 02:25:31 +0200

    update cython version

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)"
     ],
 
