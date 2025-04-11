1f8cefd10fb591d209dc09e48f050e1e26ba893b
Max Bachmann <kontakt@maxbachmann.de>
2025-04-11 13:45:54 +0200

    other files have no license header either

--- src/Levenshtein/Levenshtein-c/_levenshtein.cpp
+++ src/Levenshtein/Levenshtein-c/_levenshtein.cpp
@@ -1,30 +1,6 @@
-/*
- * Levenshtein.c
- * @(#) $Id: Levenshtein.c,v 1.41 2005/01/13 20:05:36 yeti Exp $
- * Python extension computing Levenshtein distances, string similarities,
- * median strings and other goodies.
- *
- * Copyright (C) 2002-2003 David Necas (Yeti) <yeti@physics.muni.cz>.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- **/
-
 #include <limits>
 #include <math.h>
 #include <string.h>
-/* for debugging */
 #include <stdint.h>
 #include <stdio.h>
 
